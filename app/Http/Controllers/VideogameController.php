06d6a99ff93f4190da994e0fcbc8078e537163f7
Marco-Guarnera <Darkaquon93@gmail.com>
2024-11-12 12:19:09 +0100

    Created Index Method

--- app/Http/Controllers/VideogameController.php
+++ app/Http/Controllers/VideogameController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Videogame;
 use Illuminate\Http\Request;
 
 class VideogameController extends Controller
 {
-    //
+    // Index
+    public function index() {
+        $videogames_list = Videogame::all();
+        return view('videogames.index', compact('videogames_list'));
+    }
 }
