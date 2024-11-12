79e8c1f4ae038970fba7af01dc67df5c4f47e360
FrancescoMinichiello <fraps2278@gmail.com>
2024-11-12 12:07:31 +0100

    Added videogame controller, videogame model, videogame migration and videogame seeder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +25,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home/create', [App\Http\Controllers\HomeController::class, 'create'])->name('home');
