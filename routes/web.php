e625b6cb56a790eb23c24fc84f48412e6ea909dd
Marco-Guarnera <Darkaquon93@gmail.com>
2024-11-12 12:16:46 +0100

    Created Index Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\VideogameController;
+use App\Models\Videogame;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Index
+Route::get('/videogames', [VideogameController::class, 'index'])->name('videogames.index');
